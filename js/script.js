87c1444251c2417caf3735a730b8d04ce8e17566
Ivan Sierra <sierraivan93@gmail.com>
2022-07-29 04:56:34 -0300

    Fixed all scripts

--- js/script.js
+++ js/script.js
@@ -1,96 +1,44 @@
-//Se capturan los elementos del DOM
+let registroCompras = [];
+//Luxon
+const DateTime = luxon.DateTime;
+if (localStorage.getItem(`historialTransacciones`) === null) {
+    localStorage.setItem(`historialTransacciones`, JSON.stringify(registroCompras));
+} else {
+    registroCompras = JSON.parse(localStorage.getItem(`historialTransacciones`));
+}
 
-let cryptoOption = document.getElementById(`cryptoOption`);
-let cryptoMount = document.getElementById("cryptoMount");
-let buttonCompra = document.getElementById(`buttonCompra`);
+// Funciones globales
 
-//Titulo en efecto maquina de escribir
-let text = document.getElementById(`welcomeTitle`);
-let str = text.innerHTML;
+class Historial {
+    constructor(nombre, cantidad, fecha) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+    }
 
-text.innerHTML = "";
-let speed = 150;
-let textIndex = 0;
+}
 
-typeWritter = () => {
+mostrarHistorial = () => {
 
-    if (textIndex < str.length) {
-        text.innerHTML += str.charAt(textIndex);
-        textIndex++;
-        setTimeout(typeWritter, speed);
+    let registro = registroCompras.reverse();
+
+    for (const transaccion of registro) {
+        let fila = document.createElement(`div`);
+        fila.className = `transaccionItem`
+        fila.innerHTML = `<p>Fecha: ${transaccion.fecha} - Moneda: ${transaccion.nombre} - Cantidad: ${transaccion.cantidad}</p>`;
+
+        listadoTransacciones.append(fila);
+
     }
 }
-setTimeout(typeWritter, speed);
 
-//Se crean options de monedas en el DOM
-monedasListadas.forEach(element => {
-    let option = document.createElement(`option`);
-    option.value = monedasListadas.indexOf(element);
-    option.innerText = `${element.nombre}`;
-    cryptoOption.append(option);
+obtenerFecha = (nombre, cantidad) => {
+    let fechaactual = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
 
-});
+    registroCompras.push(new Historial(nombre, cantidad, fechaactual));
 
-//Funciones
+    localStorage.setItem(`historialTransacciones`, JSON.stringify(registroCompras));
 
-buttonCompra.onclick = (e) => {
-
-    e.preventDefault();
-
-    if (cryptoMount.value < 1) {
-        Swal.fire({
-            title: 'Error en operacion',
-            text: 'Intente nuevamente.',
-            icon: 'error',
-            showConfirmButton: false,
-            timer: 2750,
-        })
-    } else {
-
-        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano&vs_currencies=usd`)
-            .then((response) => response.json())
-            .then((data) => {
-
-                let valorActual;
-                switch (+(cryptoOption.value)) {
-                    case 0:
-                        valorActual = data.bitcoin.usd;
-                        break;
-                    case 1:
-                        valorActual = data.ethereum.usd
-                        break;
-                    case 2:
-                        valorActual = data.cardano.usd
-                        break
-                }
-
-                let compraActual = cryptoMount.value / valorActual;
-
-                Swal.fire({
-                    title:`Atencion!`,
-                    text: `Vas a comprar ${compraActual} de ${(monedasListadas)[+(cryptoOption.value)].nombre} a un precio de $USD: ${valorActual} .`,
-                    icon: `warning`,
-                    showCancelButton: true,
-                    confirmButtonText: `Continuar`,
-                    cancelButtonText: `Cancelar`,
-                }).then((result) => {
-                    if(result.isConfirmed){
-                        cargarSaldo(+(cryptoOption.value), compraActual)
-
-                        obtenerFecha((monedasListadas)[+(cryptoOption.value)].nombre, compraActual);
-        
-                        Swal.fire({
-                            title: 'Operacion exitosa!',
-                            text: 'Compraste: ' + compraActual + ` de ` + (monedasListadas)[+(cryptoOption.value)].nombre,
-                            icon: 'success',
-                            showConfirmButton: false,
-                            timer: 2750,
-                        })
-                    }
-                })
-                cryptoMount.value = ``;
-            });
-    }
 }
 
 cargarSaldo = (index, cantidad) => {
@@ -100,4 +48,3 @@ cargarSaldo = (index, cantidad) => {
 
     localStorage.setItem(`monedasListadasenJSON`, JSON.stringify(monedasListadas));
 }
-
--- js/walletpage.js
+++ js/walletpage.js
@@ -3,24 +3,50 @@ let result = document.getElementById(`result`);
 
 mostrarSaldo = () => {
 
+  let saldos = JSON.parse(localStorage.getItem(`monedasListadasenJSON`));
 
-    let saldos = JSON.parse(localStorage.getItem(`monedasListadasenJSON`));
+
+  saldos.forEach(element => {
+    let walletVision = document.createElement(`div`);
+    walletVision.className = `cardContenedor`;
+    walletVision.innerHTML = `<div class="card" style="width: 16rem;">
+              <img src="../img/${element.nombre}.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+        <h5 class="card-title">${element.nombre}</h5>
+        <p class="card-text">${element.wallet}</p>
+        <p class="card-text" id="usd${element.nombre}"></p>
+      </div>
+    </div>`
+    result.append(walletVision);
+  });
 
 
-    saldos.forEach(element => {
-        let walletVision = document.createElement(`div`);
-        walletVision.className = `cardContenedor`;
-        walletVision.innerHTML = `<div class="card" style="width: 16rem;">
-                <img src="../img/${element.nombre}.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-          <h5 class="card-title">${element.nombre}</h5>
-          <p class="card-text">${element.wallet}</p>
-        </div>
-      </div>`
-      result.append(walletVision);
-    });
+  saldoEnVivo();
+
 
 }
 
-mostrarSaldo ();
+saldoEnVivo = () => {
+  fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano&vs_currencies=usd`)
+    .then((response) => response.json())
+    .then((data) => {
+      let saldos = JSON.parse(localStorage.getItem(`monedasListadasenJSON`));
+
+      let usdBitcoin = document.getElementById(`usdBitcoin`);
+      let saldoBtc = saldos[0].wallet * data.bitcoin.usd;
+      usdBitcoin.innerText = `Saldo: $ ${saldoBtc.toFixed(3)}`;
+
+      let usdEther = document.getElementById(`usdEther`);
+      let saldoEther = saldos[1].wallet * data.ethereum.usd;
+      usdEther.innerText = `Saldo: $ ${saldoEther.toFixed(3)}`;
+
+      let usdCardano = document.getElementById(`usdCardano`);
+      let saldoCardano = saldos[2].wallet * data.cardano.usd;
+      usdCardano.innerText = `Saldo: $ ${saldoCardano.toFixed(3)}`;
+    });
+}
+
+mostrarSaldo();
+
+setInterval(saldoEnVivo, 5000);
 
