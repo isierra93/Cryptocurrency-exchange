c07adef7e68249e624efa656612fd04f1d09ebcf
Ivan Sierra <sierraivan93@gmail.com>
2022-07-29 07:24:30 -0300

    Fixing img 1.0

--- js/walletpage.js
+++ js/walletpage.js
@@ -10,7 +10,7 @@ mostrarSaldo = () => {
     let walletVision = document.createElement(`div`);
     walletVision.className = `cardContenedor`;
     walletVision.innerHTML = `<div class="card" style="width: 16rem;">
-              <img src="../img/${element.nombre.toLowerCase()}.jpg" class="card-img-top" alt="">
+              <img src="../img/${element.nombre}.jpg" class="card-img-top" alt="">
               <div class="card-body">
         <h5 class="card-title">${element.nombre}</h5>
         <p class="card-text">${element.wallet}</p>
